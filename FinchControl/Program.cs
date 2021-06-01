1f2dcd72accc02f5ef1830f383696f984cb9fa00
Jason Kubo <kuboj86@gmail.com>
2021-06-01 17:22:42 -0400

    initial commit

---
+++ FinchControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinchControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
